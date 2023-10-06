7a81db219d6d9c7c3fcb21149b98002b1f7d7a5d
Alexander Holder <drowish95z@gmail.com>
2023-10-06 10:00:55 -0500

    Bug Fixes + Hints

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let score = 0;
 let gameOver = false;
 let moleTimer = 1000; //starting value, minimum 100
 let badTimer = 2100; //starting value, minimum 125
+let countdown = 25; //Checked Interval
+let currMoleTimer = 1000; //Current Vega Move Countdown
+let currBadTimer = 2100; //Current Rei Move Countdown
+let hintTimer = 5000; //How often hints refresh
+let hintShown = -1; //Hint Being Shown
 
 window.onload = function () {
     setGame();
@@ -19,14 +24,56 @@ function setGame() {
 
     setMole();
     setBad();
+    hints();
+    setInterval(actionCountdown, countdown);
+    setInterval(hints, hintTimer);
 
-    setInterval(setMole, moleTimer);
-    setInterval(setBad, badTimer);
+    //setInterval(setMole, moleTimer);
+    //setInterval(setBad, badTimer);
+}
+
+function hints() {
+    hintShown += 1;
+    if (hintShown >= 7) {
+        hintShown = 0;
+    }
+
+    if (hintShown == 0) {
+        document.getElementById("hint").innerText = "Vega is the drinking gnome. Rei is the cross-armed assassin.";
+    } else if (hintShown == 1) {
+        document.getElementById("hint").innerText = "Vega and Rei move faster after each successful click.";
+    } else if (hintShown == 2) {
+        document.getElementById("hint").innerText = "Vega and Rei may move immediately after a successful click.";
+    } else if (hintShown == 3) {
+        document.getElementById("hint").innerText = "Vega may not always be on-screen.";
+    } else if(hintShown == 4) {
+        document.getElementById("hint").innerText = "Rei may not always be on-screen.";
+    } else if (hintShown == 5) {
+        document.getElementById("hint").innerText = "This project was based on a design by Kenny Yip Coding on YouTube.";
+    } else if (hintShown == 6) {
+        document.getElementById("hint").innerText = "This project was created by Alex Holder using Javascript, CSS, and HTML.";
+    }
 }
 
 function getRandomTile() {
     let num = Math.floor(Math.random() * 9);
     return num.toString();
+}
+
+function actionCountdown() {
+    currMoleTimer -= countdown;
+    currBadTimer -= countdown;
+
+    if (currMoleTimer <= 0) {
+        setMole();
+        currMoleTimer = moleTimer;
+    }
+
+    if (currBadTimer <= 0) {
+        setBad();
+        currBadTimer = badTimer;
+    }
+
 }
 
 function setMole() {
